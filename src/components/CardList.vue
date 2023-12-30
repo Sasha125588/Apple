0692ea9ffe9dca2fa616a86acc589f497c44f79c
Sasha125588 <155098366+Sasha125588@users.noreply.github.com>
2023-12-31 00:37:25 +0300

    Add files via upload

---
+++ src/components/CardList.vue
@@ -0,0 +1,38 @@
+<script setup>
+import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert('Добавить')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-3 gap-5">
+    <Card
+      image-url="/iphons/iPhone-15-pro.jpg"
+      title="Apple iPhone 15 Pro 256GB Natural Titanium"
+      price="58 499 ₴"
+      :on-click-add="onClickAdd"
+    />
+    <Card
+      image-url="/iphons/iphone-13-pink.png"
+      title="Apple iPhone 13 128GB Pink"
+      price="29 999 ₴"
+      :on-click-add="onClickAdd"
+    />
+    <Card
+      image-url="/iphons/iPhone132.jpg"
+      title="Apple iPhone 13 128GB Midnight"
+      price="29 999 ₴"
+      :on-click-add="onClickAdd"
+    />
+    <Card
+      image-url="/iphons/iPhone-14-pro.jpg"
+      title="Apple iPhone 14 Pro 512GB Deep Purple"
+      price="62 499 ₴"
+      :on-click-add="onClickAdd"
+    />
+    <Card />
+    <Card />
+  </div>
+</template>
---
+++ src/components/CartItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex items-center border border-slate-200 p-4 rounded-xl gap-3">
+    <img class="w-16" src="/iphons/iPhone-15-pro.jpg" alt="iPhone" />
+
+    <div class="flex flex-col">
+      <p>Apple iPhone 15 Pro 256GB Natural Titanium</p>
+
+      <div class="flex justify-between mt-2">
+        <b>58 499 ₴</b>
+        <img
+          class="opacity-50 hover:opacity-100 cursor-pointer transition"
+          src="/close.svg"
+          alt="Close"
+        />
+      </div>
+    </div>
+  </div>
+</template>
